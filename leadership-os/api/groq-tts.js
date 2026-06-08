486b92f4ed9384f67d6b22004b3c1510bcdfdd27
Chessani28 <123106826+Chessani28@users.noreply.github.com>
2026-06-08 12:21:26 -0600

    Remove duplicate error log statement

--- leadership-os/api/groq-tts.js
+++ leadership-os/api/groq-tts.js
@@ -29,8 +29,9 @@ export default async function handler(req, res) {
 
   if (!response.ok) {
     const err = await response.text();
+              console.log('Entro al error jaja');
+
     console.log('TTS ERROR:', err);
-          console.log('Entro al error jaja');
 
     return res.status(response.status).send(err);
   }
